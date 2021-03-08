edb0476951d4b9ce6f01443b4660351153a2d6b3
hasanyucel <hasanyucel34@gmail.com>
2021-03-09 00:28:31 +0300

    getBatchRequestResult eklendi.

--- Trendyol/guncelle.py
+++ Trendyol/guncelle.py
@@ -12,8 +12,8 @@ jsonfile_ = "stok_guncelle.json"
 #OOP'ye göre düzenleme yapılacak.
 #Create json file methodu yazılacak.
 
-def update_stock_and_price(end_point,seller_id,user,password,jsonfile):
-    price_update_url = f"{end_point}suppliers/{seller_id}/products/price-and-inventory"
+def updatePriceAndInventory(end_point,seller_id,user,password,jsonfile):
+    url = f"{end_point}suppliers/{seller_id}/products/price-and-inventory"
     with open(jsonfile) as update_file:
         json_data = json.load(update_file)
     print(json.dumps(json_data, indent=4))
@@ -24,7 +24,22 @@ def update_stock_and_price(end_point,seller_id,user,password,jsonfile):
         "Content-Type": "application/json",
         "user-agent":f"{seller_id} - SelfIntegration"
     }
-    response = requests.request("POST", price_update_url, headers=headers, data=json.dumps(json_data))
-    print(response.text)
+    response = requests.request("POST", url, headers=headers, data=json.dumps(json_data))
+    return(response.text)
 
-update_stock_and_price(end_point_,seller_id_,user_,password_,jsonfile_)
+def getBatchRequestResult(end_point,seller_id,user,password,batchRequestId):
+    url = f"{end_point}suppliers/{seller_id}/products/batch-requests/{batchRequestId}"
+    payload={}
+    headers = {
+        "Authorization": "Basic {}".format(
+            b64encode(bytes(f"{user}:{password}", "utf-8")).decode("ascii")),
+        "Content-Type": "application/json",
+        "user-agent":f"{seller_id} - SelfIntegration"
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return(response.text)
+
+result = updatePriceAndInventory(end_point_,seller_id_,user_,password_,jsonfile_)
+result = json.loads(result)
+
+print(getBatchRequestResult(end_point_,seller_id_,user_,password_,result['batchRequestId']))
