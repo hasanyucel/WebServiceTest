88bdce4b598f8baacc81bd5337de4e8e9ead4085
hasanyucel <hasanyucel34@gmail.com>
2021-03-08 15:28:17 +0300

    update_stock_and_price fonksiyonu eklendi.

--- Trendyol/guncelle.py
+++ Trendyol/guncelle.py
@@ -2,24 +2,29 @@ import requests
 import json
 from base64 import b64encode
 
-end_point = "https://stageapi.trendyol.com/stagesapigw/" # Test: https://stageapi.trendyol.com/stagesapigw/ - Prod: https://api.trendyol.com/sapigw/
+end_point_ = "https://stageapi.trendyol.com/stagesapigw/" # Test: https://stageapi.trendyol.com/stagesapigw/ - Prod: https://api.trendyol.com/sapigw/
 
-seller_id = "2738"
-user = "LPQcjOdyyg5531DAj8J8"
-password = "H6VTAMwr2kAAIeRMfpRG"
+seller_id_ = "2738"
+user_ = "LPQcjOdyyg5531DAj8J8"
+password_ = "H6VTAMwr2kAAIeRMfpRG"
+jsonfile_ = "stok_guncelle.json"
 
-price_update_url = f"{end_point}suppliers/{seller_id}/products/price-and-inventory"
+#OOP'ye göre düzenleme yapılacak.
+#Create json file methodu yazılacak.
 
-with open('stok_guncelle.json') as json_file:
-    json_data = json.load(json_file)
-print(json.dumps(json_data, indent=4))
+def update_stock_and_price(end_point,seller_id,user,password,jsonfile):
+    price_update_url = f"{end_point}suppliers/{seller_id}/products/price-and-inventory"
+    with open(jsonfile) as update_file:
+        json_data = json.load(update_file)
+    print(json.dumps(json_data, indent=4))
 
-headers = {
-    "Authorization": "Basic {}".format(
-        b64encode(bytes(f"{user}:{password}", "utf-8")).decode("ascii")),
-    "Content-Type": "application/json",
-    "user-agent":f"{seller_id} - SelfIntegration"
-}
-response = requests.request("POST", price_update_url, headers=headers, data=json.dumps(json_data))
+    headers = {
+        "Authorization": "Basic {}".format(
+            b64encode(bytes(f"{user}:{password}", "utf-8")).decode("ascii")),
+        "Content-Type": "application/json",
+        "user-agent":f"{seller_id} - SelfIntegration"
+    }
+    response = requests.request("POST", price_update_url, headers=headers, data=json.dumps(json_data))
+    print(response.text)
 
-print(response.text)
+update_stock_and_price(end_point_,seller_id_,user_,password_,jsonfile_)
