e28d48fa791b2e7236bcea8445ab0c19746268e9
hasanyucel <hasanyucel34@gmail.com>
2021-03-10 23:21:10 +0300

    OOP Teknikleri Uygulandı.

--- Trendyol/guncelle.py
+++ Trendyol/guncelle.py
@@ -2,80 +2,79 @@ import requests
 import json
 from base64 import b64encode
 
-end_point_ = "https://stageapi.trendyol.com/stagesapigw/" # Test: https://stageapi.trendyol.com/stagesapigw/ - Prod: https://api.trendyol.com/sapigw/
-
-seller_id_ = "2738"
-user_ = "LPQcjOdyyg5531DAj8J8"
-password_ = "H6VTAMwr2kAAIeRMfpRG"
+ # Test: https://stageapi.trendyol.com/stagesapigw/ - Prod: https://api.trendyol.com/sapigw/
 jsonfile_ = "stok_guncelle.json"
-
 #OOP'ye göre düzenleme yapılacak.
 #Create json file methodu yazılacak.
 #Request Resultlarını inceleyip problemli olanları döndüren bir fonksiyon yaz.
+class Trendyol:
+    def __init__(self,endpoint,seller_id,user,password):
+        self.end_point = "https://stageapi.trendyol.com/stagesapigw/"
+        self.seller_id = seller_id
+        self.user = user
+        self.password = password
 
-def updatePriceAndInventory(end_point,seller_id,user,password,jsonfile):
-    url = f"{end_point}suppliers/{seller_id}/products/price-and-inventory"
-    with open(jsonfile) as update_file:
-        json_data = json.load(update_file)
-    print(json.dumps(json_data, indent=4))
+    def updatePriceAndInventory(self,jsonfile):
+        url = f"{self.end_point}suppliers/{self.seller_id}/products/price-and-inventory"
+        with open(jsonfile) as update_file:
+            json_data = json.load(update_file)
+        print(json.dumps(json_data, indent=4))
+        headers = {
+            "Authorization": "Basic {}".format(
+                b64encode(bytes(f"{self.user}:{self.password}", "utf-8")).decode("ascii")),
+            "Content-Type": "application/json",
+            "user-agent":f"{self.seller_id} - SelfIntegration"
+        }
+        response = requests.request("POST", url, headers=headers, data=json.dumps(json_data))
+        return(response.text)
 
-    headers = {
-        "Authorization": "Basic {}".format(
-            b64encode(bytes(f"{user}:{password}", "utf-8")).decode("ascii")),
-        "Content-Type": "application/json",
-        "user-agent":f"{seller_id} - SelfIntegration"
-    }
-    response = requests.request("POST", url, headers=headers, data=json.dumps(json_data))
-    return(response.text)
+    def checkBatchRequestResult(self,batchRequestId):
+        url = f"{self.end_point}suppliers/{self.seller_id}/products/batch-requests/{batchRequestId}"
+        payload={}
+        headers = {
+            "Authorization": "Basic {}".format(
+                b64encode(bytes(f"{self.user}:{self.password}", "utf-8")).decode("ascii")),
+            "Content-Type": "application/json",
+            "user-agent":f"{self.seller_id} - SelfIntegration"
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return(response.text)
 
-def checkBatchRequestResult(end_point,seller_id,user,password,batchRequestId):
-    url = f"{end_point}suppliers/{seller_id}/products/batch-requests/{batchRequestId}"
-    payload={}
-    headers = {
-        "Authorization": "Basic {}".format(
-            b64encode(bytes(f"{user}:{password}", "utf-8")).decode("ascii")),
-        "Content-Type": "application/json",
-        "user-agent":f"{seller_id} - SelfIntegration"
-    }
-    response = requests.request("GET", url, headers=headers, data=payload)
-    return(response.text)
+    def getApprovedProducts(self):
+        url = f"{self.end_point}suppliers/{self.seller_id}/products?page=1&size=2&approved=true"
+        payload={}
+        headers = {
+            "Authorization": "Basic {}".format(
+                b64encode(bytes(f"{self.user}:{self.password}", "utf-8")).decode("ascii")),
+            "Content-Type": "application/json",
+            "user-agent":f"{self.seller_id} - SelfIntegration"
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return(response.text)
 
-def getApprovedProducts(end_point,seller_id,user,password):
+    def getProductListPrice(self,barcode):
+        url = f"{self.end_point}suppliers/{self.seller_id}/products?approved=true&barcode={barcode}"
+        payload={}
+        headers = {
+            "Authorization": "Basic {}".format(
+                b64encode(bytes(f"{self.user}:{self.password}", "utf-8")).decode("ascii")),
+            "Content-Type": "application/json",
+            "user-agent":f"{self.seller_id} - SelfIntegration"
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+        employee_dict = json.loads(response.text) 
+        print("List Price: ",employee_dict["content"][0]["listPrice"])
+        print("Sale Price: ",employee_dict["content"][0]["salePrice"])
 
-    url = f"{end_point}suppliers/{seller_id}/products?page=1&size=2&approved=true"
-
-    payload={}
-    headers = {
-        "Authorization": "Basic {}".format(
-            b64encode(bytes(f"{user}:{password}", "utf-8")).decode("ascii")),
-        "Content-Type": "application/json",
-        "user-agent":f"{seller_id} - SelfIntegration"
-    }
-
-    response = requests.request("GET", url, headers=headers, data=payload)
-
-    return(response.text)
-
-def getProductListPrice(end_point,seller_id,user,password,barcode):
-
-    url = f"{end_point}suppliers/{seller_id}/products?approved=true&barcode={barcode}"
-    payload={}
-    headers = {
-        "Authorization": "Basic {}".format(
-            b64encode(bytes(f"{user}:{password}", "utf-8")).decode("ascii")),
-        "Content-Type": "application/json",
-        "user-agent":f"{seller_id} - SelfIntegration"
-    }
-    response = requests.request("GET", url, headers=headers, data=payload)
-    employee_dict = json.loads(response.text) 
-    print("List Price: ",employee_dict["content"][0]["listPrice"])
-    print("Sale Price: ",employee_dict["content"][0]["salePrice"])
-
-getProductListPrice(end_point_,seller_id_,user_,password_,"2100000078727")
+#getProductListPrice(end_point_,seller_id_,user_,password_,"2100000078727")
 #products = getApprovedProducts(end_point_,seller_id_,user_,password_)
 #print(json.dumps(json.loads(products), sort_keys=True, indent=4, separators=(",", ": ")))
-
 #result = updatePriceAndInventory(end_point_,seller_id_,user_,password_,jsonfile_)
 #result = json.loads(result)
-
 #print(checkBatchRequestResult(end_point_,seller_id_,user_,password_,result['batchRequestId']))
+#seller_id = "2738"
+#user = "LPQcjOdyyg5531DAj8J8"
+#password = "H6VTAMwr2kAAIeRMfpRG"
+user1 = Trendyol('','2738','LPQcjOdyyg5531DAj8J8','H6VTAMwr2kAAIeRMfpRG')
+products = user1.getApprovedProducts()
+print(products)
