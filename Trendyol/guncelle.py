b9586203d33ab5307f96144af0bf67cd206d0c15
hasanyucel <hasanyucel34@gmail.com>
2021-03-12 23:23:05 +0300

    createUpdateJsonFileFromList fonksiyonu eklendi.

--- Trendyol/guncelle.py
+++ Trendyol/guncelle.py
@@ -1,11 +1,14 @@
 import requests
 import json
+import collections
 from base64 import b64encode
+import sqlite3 #Test
+import timeit
 
 # Test: https://stageapi.trendyol.com/stagesapigw/ - Prod: https://api.trendyol.com/sapigw/
 test = "https://stageapi.trendyol.com/stagesapigw/"
 prod = "https://api.trendyol.com/sapigw/"
-
+db = "etipaen.db"
 class Trendyol:
     def __init__(self,end_point,seller_id,user,password):
         self.end_point = end_point
@@ -65,17 +68,33 @@ class Trendyol:
         print("List Price: ",employee_dict["content"][0]["listPrice"])
         print("Sale Price: ",employee_dict["content"][0]["salePrice"])
 
-    #Create json file methodu yazılacak.
+    def createUpdateJsonFileFromList(self,lst):
+        objects_dict = {}
+        objects_dict["items"] = []
+        for row in lst:
+            d = collections.OrderedDict()
+            d["barcode"] = row[0]
+            d["listPrice"] = row[1]
+            d["salePrice"] = row[2]
+            objects_dict["items"].append(d)
+
+        j = json.dumps(objects_dict,indent=4)
+        with open("productUpdate.json", "w") as f:
+            f.write(j)
+
+
     #Request Resultlarını inceleyip problemli olanları döndüren bir fonksiyon yaz.
 
-#jsonfile = "stok_guncelle.json"
 #seller_id = "2738"
 #user = "LPQcjOdyyg5531DAj8J8"
 #password = "H6VTAMwr2kAAIeRMfpRG"
-#user1 = Trendyol(test,'2738','LPQcjOdyyg5531DAj8J8','H6VTAMwr2kAAIeRMfpRG')
+user1 = Trendyol(test,'2738','LPQcjOdyyg5531DAj8J8','H6VTAMwr2kAAIeRMfpRG')
 #products = user1.updatePriceAndInventory(jsonfile)
 #print(products)
 #user1.getProductListPrice("1952084972279")
-        
 
-    
+start = timeit.timeit()
+productList = [('1952084972279', 100.5, 99), ('1952084972280', 115.3, 100)]
+user1.createUpdateJsonFileFromList(productList)
+end = timeit.timeit()
+print(start - end)
